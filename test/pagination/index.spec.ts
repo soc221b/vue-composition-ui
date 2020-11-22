a056b9adc6767b2f19dd82236136e8fe6d175162
Ernest <iendeavor.github.io@gmail.com>
2020-11-22 17:53:00 +0800

    test: add test

---
+++ test/pagination/index.spec.ts
@@ -0,0 +1,32 @@
+import { ref } from 'vue'
+import { usePagination } from '../../'
+
+it('should keep reactivity', () => {
+  const originalCurrentPage = ref(1)
+  const originalPerPageSize = ref(10)
+  const originalTotalSize = ref(42)
+
+  const { currentPage, perPageSize, totalSize } = usePagination({
+    currentPage: originalCurrentPage,
+    perPageSize: originalPerPageSize,
+    totalSize: originalTotalSize,
+  })
+
+  expect(currentPage.value).toBe(1)
+  expect(perPageSize.value).toBe(10)
+  expect(totalSize.value).toBe(42)
+
+  originalCurrentPage.value = 10
+  originalPerPageSize.value = 100
+  originalTotalSize.value = 420
+  expect(currentPage.value).toBe(10)
+  expect(perPageSize.value).toBe(100)
+  expect(totalSize.value).toBe(420)
+
+  currentPage.value = 1
+  perPageSize.value = 10
+  totalSize.value = 42
+  expect(originalCurrentPage.value).toBe(1)
+  expect(originalPerPageSize.value).toBe(10)
+  expect(originalTotalSize.value).toBe(42)
+})
