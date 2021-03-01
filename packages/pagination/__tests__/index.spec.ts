a1253f97e0d3e924d1252992f0482b5e15c6d5dc
Ernest <iendeavor.github.io@gmail.com>
2021-03-01 17:57:34 +0800

    test: rearrange directory structure

--- packages/pagination/__tests__/index.spec.ts
+++ packages/pagination/__tests__/index.spec.ts
@@ -1,5 +1,5 @@
 import { ref } from 'vue'
-import { usePagination } from '../../packages/pagination'
+import { usePagination } from '../src'
 
 it('should keep reactivity', () => {
   const originalCurrentPage = ref(1)
